acae01890e405f74f2d6ff54eb6a8f1fea3f85c9
Rafael_s1lva <ngc.rafs@outlook.com>
2024-09-19 21:24:41 -0300

    Segundo commit

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from'./Lista.module.scss';
+import Item from './Item/index';
 
 function Lista(){
-    const tarefas = [{
+    const [tarefas, setTarefas] = useState([{
         tarefa: 'React',
         tempo: '02:00:00'
     } ,{
         tarefa: 'Javascript',
         tempo: '01:00:00'
-    }]
+    } ,{
+        tarefa: 'Typescript',
+        tempo: '03:00:00'
+    }])
     return(
-        <aside className={style.Lista}>
-            <h2> Estudos do dia</h2>
+        <aside className={style.listaTarefas}>
+            <h2 onClick={()=>{
+                setTarefas([...tarefas])
+            }}> Estudos do dia</h2>
             <ul>
                 {tarefas.map((item, index) =>(
-                    <li key = {index} className={style.item}>
-                        <h3> {item.tarefa} </h3>
-                        <span> {item.tempo} </span>
-                    </li>
+                 <Item
+                 key={index}
+                 {...item}
+                 />
                 ))}
             </ul>
         </aside>
--- src/components/botao/index.tsx
+++ src/components/botao/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import style from "./Botao.module.scss";
-class Botao extends React.Component{
-    render() {
-        return(
-            <button className={style.botao}>
-                Botão
-            </button>
-        )
-    }
+import style from './Botao.module.scss';
+
+interface BotaoProps {
+  children: React.ReactNode;
 }
+
+class Botao extends React.Component<BotaoProps> {
+  render() {
+    return (
+      <button className={style.botao}>
+        {this.props.children}
+      </button>
+    );
+  }
+}
+
 export default Botao;
